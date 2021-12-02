4884f250bb5694007651f09677897293e215fe2b
Raadiitz <bengkululegend7@gmail.com>
2021-12-02 16:02:04 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,270 @@
+<!doctype html>
+ <html class="no-js" lang=""> <!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>WebProfil - Raditya</title>
+
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="asset/apple-touch-icon.png">
+
+    <link rel="stylesheet" href="asset/css/bootstrap.min.css">
+    <link rel="stylesheet" href="asset/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="asset/css/templatemo-style.css">
+
+    <script src="asset/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+  </head>
+  <body>
+
+    <nav class="nav">
+      <div class="burger">
+        <div class="burger__patty"></div>
+      </div>
+
+      <ul class="nav__list">
+        <li class="nav__item">
+          <a href="#1" class="nav__link c-blue"><img src="asset/img/home-icon.png" alt=""></a>
+        </li>
+        <li class="nav__item">
+          <a href="#2" class="nav__link c-yellow scrolly"><img src="asset/img/about-icon.png" alt=""></a>
+        </li>
+        <li class="nav__item">
+          <a href="#3" class="nav__link c-red"><img src="asset/img/projects-icon.png" alt=""></a>
+        </li>
+        <li class="nav__item">
+          <a href="#4" class="nav__link c-green"><img src="asset/img/contact-icon.png" alt=""></a>
+        </li>
+      </ul>
+    </nav>
+
+    <section class="panel b-blue" id="1">
+      <article class="panel__wrapper">
+        <div class="panel__content">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-8 col-md-offset-2">
+                <div class="home-content">
+                  <div class="home-heading">
+                    <h1><em>PortPolio</em> Raditya</h1>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="home-box-content">
+                        <div class="left-text">
+                          <h4>Halo <em>Saya</em> Raditya</h4>
+                          <p>Nama saya Raditya. Saya adalah siswa di SMK Negeri 01 Kota Bengkulu, Jurusan yang saya ambil adalah Rekayasa Perangkat Lunak.</p>
+                          <div class="primary-button">
+                            <a href="https://www.instagram.com/raadiitz/">Tekan Saja</a>
+                          </div>
+                        </div>
+                        <div class="right-image">
+                          <img src="asset/img/o.png" alt="">
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </article>
+    </section>
+    <section class="panel b-yellow" id="2">
+      <article class="panel__wrapper">
+        <div class="panel__content">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-8 col-md-offset-2">
+                <div class="about-content">
+                  <div class="heading">
+                    <h4>About us</h4>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="about-box-content">
+                        <img src="asset/img/bg.png" alt="">
+                      </div>
+                    </div>
+                    <div class="col-md-7 col-md-offset-5">
+                      <div class="about-box-text">
+                        <h4>Tentang Diri Saya</h4>
+                        <p>Saya adalah seorang siswa smk di SMK Negeri 01 Kota Bengkulu. Saya memiliki hobi bermain game dan bermain basket. Saya tertarik belajar Design dan Ngoding, untuk design mungkin saya lebih suka design semacam banner.</p>
+                        <div class="primary-button">
+                          <a href="https://www.instagram.com/raadiitz/">Tekan Saja</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </article>
+    </section>
+    <section class="panel b-red" id="3">
+      <article class="panel__wrapper">
+        <div class="panel__content">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-8 col-md-offset-2">
+                <div class="projects-content">
+                  <div class="heading">
+                    <h4>Project Saya</h4>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="owl-carousel owl-theme projects-container">
+                        <div>
+                          <div class="project-item">
+                            <a href="project-item/asset/img/1.png" data-lightbox="image-1"><img src="asset/img/1.png" alt=""></a>
+                            <div class="text-content">
+                              <h4>AlfamartClass</h4>
+                              <p>Website AlfamartClass yang menyediakan produk berupa kebutuhan sehari hari untuk masyarakat.</p>
+                              <div class="primary-button">
+                                <a href="https://www.instagram.com/raadiitz/">Tekan Saja</a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div>
+                          <div class="project-item">
+                            <a href="asset/img/2.png" data-lightbox="image-1"><img src="asset/img/2.png" alt=""></a>
+                            <div class="text-content">
+                              <h4>Admin Website</h4>
+                              <p>Website Admin yang digunakan untuk menambahkan produk, menampung data user, dan lain lain pada AlfamartClass.</p>
+                              <div class="primary-button">
+                                <a href="https://www.instagram.com/raadiitz/">Tekan Saja</a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div>
+                          <div class="project-item">
+                            <a href="asset/img/3.png" data-lightbox="image-1"><img src="asset/img/3.png" alt=""></a>
+                            <div class="text-content">
+                              <h4>Buku Tamu</h4>
+                              <p>Website pengisian buku tamu yang saya buat saat magang di PT Ascon Inovasi Data.</p>
+                              <div class="primary-button">
+                                <a href="https://www.instagram.com/raadiitz/">Tekan Saja</a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div>
+                          <div class="project-item">
+                            <a href="asset/img/4.png" data-lightbox="image-1"><img src="asset/img/4.png" alt=""></a>
+                            <div class="text-content">
+                              <h4>Banner Ascon</h4>
+                              <p>Banner PT Ascon Inovasi Data yang saya buat saat magang.</p>
+                              <div class="primary-button">
+                                <a href="https://www.instagram.com/raadiitz/">Tekan Saja</a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div>
+                          <div class="project-item">
+                            <a href="asset/img/5.png" data-lightbox="image-1"><img src="asset/img/5.png" alt=""></a>
+                            <div class="text-content">
+                              <h4>Banner Scaleupin</h4>
+                              <p>Banner PT Ascon Inovasi Data yang saya buat saat magang.</p>
+                              <div class="primary-button">
+                                <a href="https://www.instagram.com/raadiitz/">Tekan Saja</a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div>
+                          <div class="project-item">
+                            <a href="asset/img/6.png" data-lightbox="image-1"><img src="asset/img/6.png" alt=""></a>
+                            <div class="text-content">
+                              <h4>Banner Oasse</h4>
+                              <p>Banner PT Ascon Inovasi Data yang saya buat saat magang.</p>
+                              <div class="primary-button">
+                                <a href="https://www.instagram.com/raadiitz/">Tekan Saja</a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </article>
+    </section>
+    <section class="panel b-green" id="4">
+      <article class="panel__wrapper">
+        <div class="panel__content">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-8 col-md-offset-2">
+                <div class="contact-content">
+                  <div class="heading">
+                    <h4>Contact Saya</h4>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-8">
+                      <div class="contat-form">
+                        <form id="contact" action="" method="post">
+                          <fieldset>
+                            <input name="name" type="text" class="form-control" id="name" placeholder="Nama Anda" required>
+                          </fieldset>
+                          <fieldset>
+                            <input name="email" type="email" class="form-control" id="email" placeholder="Email" required>
+                          </fieldset>
+                          <fieldset>
+                            <textarea name="message" rows="6" class="form-control" id="message" placeholder="Pesan" required></textarea>
+                          </fieldset>
+                          <fieldset>
+                            <button type="submit" id="form-submit" class="btn">Kirim Pesan</button>
+                          </fieldset>
+                        </form>
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                      <div class="more-info">
+                        <p>Anda dapat menghubungi Saya melalui halaman Contact tersebut. Terima Kasih.<br><br>
+                          <em>2021 Raditya,
+                            <br>Jl. Hibrida Raya Bengkulu, 38229</em></p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-12">
+                    <div class="footer">
+                      <p>Copyright &copy; 2021 Raditya 
+                      | SMK Negeri 01 Kota Bengkulu</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </article>
+        </section>
+
+
+        <script src="asset///ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="asset/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+          <script src="asset/js/vendor/bootstrap.min.js"></script>
+
+          <script src="asset/js/plugins.js"></script>
+          <script src="asset/js/main.js"></script>
+          <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+              function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+          </script>
+        </body>
+        </html>
